b7cb73918b606accb23a80167ae51d359295f9e1
Luc Ostman <peolina@live.com.au>
2022-11-29 18:44:53 +1100

    added activity for lesson

---
+++ async-js/exercise/activity.js
@@ -0,0 +1,20 @@
+/*
+* Problem
+It is your birthday and you want to celebrate it with all your friends at the park.
+But for that to happen, the weather needs to be good. No raining.
+If the weather is not good, your birthday party will happen in your tiny apartment.
+Unfortunately, you can't invite all your friends if your birthday party needs to be in your house.
+
+How can your create a Promise for that situation?
+Start defining the weather. Is it raining?
+Then write a promise (create a promise).
+Then check the promise (consume the promise).
+*/
+
+//Define the weather as a variable and set it to a boolean value.
+
+//Create a promise. 
+// (hint: you may find revising the theory on W3Schools helps: 
+// https://www.w3schools.com/js/js_promise.asp)
+
+//Consume the promise using then() and catch().
