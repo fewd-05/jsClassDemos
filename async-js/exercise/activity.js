09f19bda449b45aad45aaf4240b7dc5021cffe67
Luc Ostman <peolina@live.com.au>
2022-12-01 13:19:40 +1100

    added classes work

--- async-js/exercise/activity.js
+++ async-js/exercise/activity.js
@@ -12,9 +12,26 @@ Then check the promise (consume the promise).
 */
 
 //Define the weather as a variable and set it to a boolean value.
+let isRaining = true;
 
 //Create a promise. 
 // (hint: you may find revising the theory on W3Schools helps: 
 // https://www.w3schools.com/js/js_promise.asp)
+const birthday = new Promise((resolve,reject) => {
+    if (isRaining == false) {
+        let venue = {
+            location: "Centennial Park",
+            attendees: 100
+        }
+        resolve(venue)
+    } else {
+        reject('Birthday in the park cancelled!')
+    }
+});
 
 //Consume the promise using then() and catch().
+birthday.then(function(venue) {
+    console.log(venue);
+}).catch(function(err) {
+    console.log(err);
+})
--- async-js/index.js
+++ async-js/index.js
@@ -1,26 +1,39 @@
 // promise example
-let phone = true;
+let groceriesDone = false;
 
-const iHaveAPhone = new Promise(function(resolve,reject) {
-    if (phone == true) {
-        resolve("You own a phone!")
+const iHaveDoneGroceries = new Promise((resolve,reject) => {
+    if (groceriesDone) {
+        resolve('You can finally watch some TV!');
     } else {
-        reject("You need to buy a phone.")
+        reject('You need to do groceries!');
     }
+});
+
+iHaveDoneGroceries.then(function(res) {
+    console.log(res);
+}).catch(function(penguin) {
+    console.log(penguin)
 })
 
-iHaveAPhone.then(function(resolution) {
-    console.log(resolution);
-}).catch(function(error) {
-    console.log(error);
+// fetch example
+console.log("start");
+
+fetch('https://api.chucknorris.io/jokes/random?category=travel')
+.then(function(res){
+    return res.json();
+}).then(function(res) {
+    console.log(res);
 });
 
-// fetch example
-fetch("https://api.imgflip.com/get_memes")
-.then((res) => {return res.json()})
-.then((res) => {console.log(res.data.memes[0].url);
-    let url = res.data.memes[0].url;
-    let meme = document.createElement('img');
-    meme.setAttribute('src', url);
-    document.body.appendChild(meme);
-});
+console.log("end");
+
+// fetch("https://api.imgflip.com/get_memes")
+// .then((res) => {return res.json()})
+// .then((res) => {console.log(res.data.memes[0].url);
+//     let url = res.data.memes[0].url;
+//     let meme = document.createElement('img');
+//     meme.setAttribute('src', url);
+//     document.body.appendChild(meme);
+// });
+
+
